07f50ce4309e2aee2bb24bedcf734617b7273c60
Anatoliy Krekotenko <krekotenko@gmail.com>
2019-03-01 17:51:10 +0200

    Add europe shipping

---
+++ admin/language/cz-cz/extension/shipping/flat2.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_title']       = 'v Evropě';
+$_['text_description'] = 'v Evropě';
---
+++ admin/language/en-gb/extension/shipping/flat2.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']    = 'Europe';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified Europe!';
+$_['text_edit']        = 'Edit Europe';
+
+// Entry
+$_['entry_cost']       = 'Cost';
+$_['entry_sum_free_shipping'] = 'Min sum for free shipping';
+$_['entry_tax_class']  = 'Tax Class';
+$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Europe!';
